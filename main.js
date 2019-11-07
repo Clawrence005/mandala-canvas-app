dadb9decfb6ae43a99dadfdccc23ae00b0082c91
colin <clawrence005@yahoo.com>
2019-11-07 01:36:38 -0600

    created stopDrawing and recordPointerLocation funcs

--- main.js
+++ main.js
@@ -13,4 +13,36 @@ const init = () => {
   context = canvas.getContext("2d");
   w = canvas.width;
   h = canvas.height;
+
+  canvas.onpointermove = handlePointerMove;
+  canvas.onpointerdown = handlePointerDown;
+  canvas.onpointerup = stopDrawing;
+  canvas.onpointerout = stopDrawing;
+}
+// creates the original line from the pointer path
+const drawLine = () => {
+  let a = prevX,
+    b = prevY,
+    c = currX,
+    d = currY;
+
+  context.lineWidth = 4;
+  context.lineCap = "round";
+
+  context.beginPath();
+  context.moveTo(a, b);
+  context.lineTo(c, d);
+  context.stroke();
+  context.closePath();
+}
+
+const stopDrawing = () => {
+  draw = false;
+}
+
+const recordPointerLocation = (e) => {
+  prevX = currX;
+  prevY = currY;
+  currX = e.clientX - canvas.offsetLeft;
+  currY = e.clientY - canvas.offsetTop;
 }
